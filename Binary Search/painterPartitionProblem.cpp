ba34a084b9db13097c04d8a0427c40ce0a71dfc8
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-06-09 00:45:48 +0530

    MAQ Questions

---
+++ Binary Search/painterPartitionProblem.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    bool isPossibleSolution(int a[], int n, int k, long long mid)
+    {
+        long long timeSum = 0;
+        int c = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > mid)
+            {
+                return false;
+            }
+            if (a[i] + timeSum > mid)
+            {
+                c++;
+                timeSum = a[i];
+                if (c > k)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                timeSum += a[i];
+            }
+        }
+        return true;
+    }
+    long long minTime(int arr[], int n, int k)
+    {
+        long long start = 0;
+        long long end = 0;
+        for (int i = 0; i < n; i++)
+        {
+            end += arr[i];
+        }
+        long long ans = -1;
+        while (start <= end)
+        {
+            long long mid = start + (end - start) / 2;
+            if (isPossibleSolution(arr, n, k, mid))
+            {
+                ans = mid;
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cout << "Enter the size of loop :- ";
+    cin >> t;
+    while (t--)
+    {
+        int k, n;
+        cout << "Enter the size painters :- ";
+        cin >> k;
+        cout << "Enter the size of array :- ";
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Enter the array elements :- ";
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minTime(arr, n, k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends 5,10,30,20,15
