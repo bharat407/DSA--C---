bdc463acbb6e5c6d63dc4db114aff3e803d21e06
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-06-11 10:22:12 +0530

    Pointers

--- Pointers/Pointers_With_Functions_1.cpp
+++ Pointers/Pointers_With_Functions_1.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void solve(int *p){
+void solve(int *p)
+{
     *p = *p + 10; // value at address stored in p is incremented by 10
 }
-int main(){
+int main()
+{
     int a = 5;
-    int *ptr = &a; // Pointer to integer variable a 
-    solve(ptr); // Pointer is passed to the function
-    cout<<a; // modified value will be printed i.e. 15
+    int *ptr = &a; // Pointer to integer variable a
+    solve(ptr);    // Pointer is passed to the function
+    cout << a;     // modified value will be printed i.e. 15
     return 0;
 }
