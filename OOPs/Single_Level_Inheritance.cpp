c95add224f1e7ebf36b96678fff67181a026204f
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-06-21 11:56:49 +0530

    OOPs Inheritance

---
+++ OOPs/Single_Level_Inheritance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Car
+{
+public:
+    string name;
+    int weight;
+    int age;
+    void speedup()
+    {
+        cout << "Speeding up";
+    }
+    void brake()
+    {
+        cout << "Brake";
+    }
+};
+class Rapid : public Car
+{
+};
+int main()
+{
+
+    return 0;
+}
