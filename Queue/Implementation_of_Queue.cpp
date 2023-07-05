5d32857e37b38e0cb72e7a3d05221cf2e5a02541
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-07-05 11:37:23 +0530

    Update

--- Queue/Implementation_of_Queue.cpp
+++ Queue/Implementation_of_Queue.cpp
@@ -95,7 +95,7 @@ int main()
     q.pop();
     cout << "Size of queue = " << q.getSize() << endl;
     // Getting front element
-    cout << "Front element = " << q.getFront();
+    cout << "Front element = " << q.getFront() << endl;
     // Checking empty
     if (q.isEmpty())
     {
